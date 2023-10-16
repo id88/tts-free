d97edab2724e0289591d668ed379b26c899b5545
id88 <isisyouid@gmail.com>
2023-10-16 21:25:43 +0800

    添加最小化到托盘

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
+import { app, BrowserWindow, shell, ipcMain, Tray, Menu, dialog, nativeImage } from 'electron'
 import { release } from 'node:os'
 import { join } from 'node:path'
 
@@ -39,16 +39,19 @@ let win: BrowserWindow | null = null
 const preload = join(__dirname, '../preload/index.js')
 const url = process.env.VITE_DEV_SERVER_URL
 const indexHtml = join(process.env.DIST, 'index.html')
+const iconPath = join(process.env.VITE_PUBLIC, 'tts-free.png')
 
-async function createWindow() {
+function createWindow() {
     win = new BrowserWindow({
         width: 900,
         height: 650,
         minWidth: 900,
         minHeight: 650,
         title: 'Main window',
-        // frame: false,
-        icon: join(process.env.VITE_PUBLIC, 'tts-free.png'),
+        // frame: false, // 无边框
+        // transparent: true, // 窗体透明
+        // backgroundColor: '#ffc',
+        icon: iconPath,
         webPreferences: {
             preload,
             // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
@@ -81,13 +84,75 @@ async function createWindow() {
         return { action: 'deny' }
     })
     // win.webContents.on('will-navigate', (event, url) => { }) #344
+
+    return win;
 }
 
-app.whenReady().then(createWindow)
+// app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+    const mainWindow = createWindow();
+    //监听关闭事件
+    mainWindow.on('close', () => {
+        console.log('close.....')
+    })
+    // const tray = new Tray(iconPath)  // 系统托盘图标
+    const icon = nativeImage.createFromPath(iconPath)
+    const tray = new Tray(icon)
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: '打开tts-free',
+            click: function() {
+                win.show()
+            }
+        },
+        {
+            label: '设置',
+            click: function () {
+                console.log('设置.....')
+            }
+        },
+        {
+            label: '退出',
+            icon:  join(process.env.VITE_PUBLIC, './tray/exit.png'),
+            click: () => {
+                app.exit(0);
+            }
+        }
+    ])
+    // 当鼠标移动到小图标上方时显示的提示
+    tray.setToolTip('This is my application.')
+    tray.setContextMenu(contextMenu)
+
+    // 点击托盘图标时
+    tray.on('click', () =>{
+        win.show();
+    })
+
+    win.on("close", (event) => {
+        if (dialog.showMessageBoxSync(win, {
+            type: "info",
+            title: "提示",
+            message: "确定要退出吗？",
+            buttons: ["最小化到托盘", "直接退出"],
+            defaultId: 0,
+            cancelId: 1
+        }) === 0) {
+            event.preventDefault();
+            win.hide();
+        } else {
+            app.exit();
+        }
+    });
+})
 
 app.on('window-all-closed', () => {
     win = null
     if (process.platform !== 'darwin') app.quit()
+    // 清理以下对象：
+    // 1. 主窗口（BrowserWindow）：如果你创建了一个或多个主窗口，当所有主窗口都关闭时，可以调用 app.quit() 来退出应用程序。
+    // 2. 托盘图标（Tray）：如果你创建了托盘图标，当所有主窗口关闭并且应用程序即将退出时，可以调用 tray.destroy() 方法来销毁托盘图标对象。
+    // 3. 自定义菜单（Menu）：如果你创建了自定义菜单，当所有主窗口关闭并且应用程序即将退出时，可以调用 Menu.setApplicationMenu(null) 来清除应用程序的菜单栏。
+    // 4. 其他需要手动清理的自定义对象：如果你在应用程序中使用了其他自定义对象，例如自定义计时器、网络连接等
 })
 
 app.on('second-instance', () => {
@@ -107,18 +172,18 @@ app.on('activate', () => {
     }
 })
 
-ipcMain.on("min", (e) => win.minimize());
-ipcMain.on("window-maximize", function () {
-    if (win.isFullScreen()) {
-        win.setFullScreen(false);
-    } else if (win.isMaximized()) {
-        win.unmaximize();
-    } else {
-        win.maximize();
-    }
-});
-ipcMain.on("close", (e) => win.close());
-ipcMain.on("reload", (e) => win.reload());
+// ipcMain.on("min", (e) => win.minimize());
+// ipcMain.on("window-maximize", function () {
+//     if (win.isFullScreen()) {
+//         win.setFullScreen(false);
+//     } else if (win.isMaximized()) {
+//         win.unmaximize();
+//     } else {
+//         win.maximize();
+//     }
+// });
+// ipcMain.on("close", (e) => win.close());
+// ipcMain.on("reload", (e) => win.reload());
 
 
 
